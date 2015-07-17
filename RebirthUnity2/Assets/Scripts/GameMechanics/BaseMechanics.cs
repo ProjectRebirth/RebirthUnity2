44e42580787bc97ab308882dac76a24acfed14ac
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-07-17 16:15:29 -0400

    BaseMechanics and scripts added

---
+++ RebirthUnity2/Assets/Scripts/GameMechanics/BaseMechanics.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseMechanics : MonoBehaviour {
+	public BaseSpriteStats baseStats;
+	private float goalWalkSpeed;
+
+	// Use this for initialization
+	protected virtual void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+	
+	}
+
+	protected virtual void FixedUpdate() {
+
+	}
+
+	public virtual void moveHorizontal(float horizontalInput) {
+
+	}
+
+	public virtual void moveVertical(float verticalInput) {
+
+	}
+
+	protected virtual void updateHorizontalMovement() {
+
+	}
+
+	protected virtual void updateVerticalMovement() {
+
+	}
+
+
+}
---
+++ RebirthUnity2/Assets/Scripts/GameStatistics/BaseSpriteStats.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseSpriteStats : MonoBehaviour{
+	public float maxHealth;//The maximum health that the sprite can hold at any given time
+	public float curHealth;//The current health that the sprite has at any given time
+	public float horizontalSpeed;
+	public float jumpSpeed;//The speed that will be immediately set to the y velocity
+	public bool isRight;
+	private bool inAir;
+	private bool isWalking;
+
+
+	protected virtual void Update() {
+
+	}
+
+	protected virtual void Start() {
+
+	}
+
+	protected virtual void FixedUpdate() {
+
+	}
+
+	public bool getInAir() {
+		return inAir;
+	}
+
+	public virtual void setInAir(bool inAir) {
+		this.inAir = inAir;
+	}
+
+	public bool getIsWalking() {
+		return isWalking;
+	}
+
+	public virtual void setIsWalking(bool isWalking) {
+		this.isWalking = isWalking;
+	}
+}
+
+

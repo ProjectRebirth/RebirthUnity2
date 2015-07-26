c321037566e5d99b1be2e6266131892d0d592bdf
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-07-26 15:04:13 -0400

    Bullet Animations complete

---
+++ RebirthUnity2/Assets/Scripts/AnimationScripts/Weapons/Projectile/BulletAssaultAnimation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletAssaultAnimation : MonoBehaviour {
+	public Animator bulletAnimator;
+	public ProjectileStats bulletStats;
+	
+	// Update is called once per frame
+	void Update () {
+		print (bulletStats.getHasCollided ());
+		bulletAnimator.SetBool ("hasCollided", bulletStats.getHasCollided ());
+	}
+}

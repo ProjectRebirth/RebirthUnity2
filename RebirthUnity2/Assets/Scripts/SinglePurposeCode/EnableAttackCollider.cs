ae97db316303271db8f91a87017094e5783f7667
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-08-11 14:58:33 -0400

    DelayAttack added
    New Skeleton prefab

--- RebirthUnity2/Assets/Scripts/SinglePurposeCode/EnableAttackCollider.cs
+++ RebirthUnity2/Assets/Scripts/SinglePurposeCode/EnableAttackCollider.cs
@@ -4,13 +4,26 @@ using System.Collections;
 public class EnableAttackCollider : MonoBehaviour {
 	public Collider2D attackCollider;
 	public MeleeWeaponStats meleeStats;
-
+	public float delayEnable;//The amount of time when the sprite begins his attack and when the box will be active for attack.
+	private float delayEnableTimer;
 
 	void Update() {
+		updateDelayEnableTimer ();
 		if (meleeStats.getIsAttacking ()) {
-			attackCollider.enabled = true;
+			if (delayEnableTimer <= 0) {
+				attackCollider.enabled = true;
+			}
+
 		} else {
 			attackCollider.enabled = false;
+			delayEnableTimer = delayEnable;
+		}
+	}
+
+	void updateDelayEnableTimer() {
+		delayEnableTimer -= Time.deltaTime;
+		if (delayEnableTimer < 0) {
+			delayEnableTimer = 0f;
 		}
 	}
 }

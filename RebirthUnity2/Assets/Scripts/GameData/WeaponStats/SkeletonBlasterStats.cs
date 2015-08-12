fb6ae1b6c2321ec579f4c5d16e6261d190983441
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-08-11 21:51:59 -0400

    Skeleton Mechanics complete

--- RebirthUnity2/Assets/Scripts/GameData/WeaponStats/SkeletonBlasterStats.cs
+++ RebirthUnity2/Assets/Scripts/GameData/WeaponStats/SkeletonBlasterStats.cs
@@ -2,27 +2,31 @@ using UnityEngine;
 using System.Collections;
 
 public class SkeletonBlasterStats : BaseGunStats {
-	public float nextShotTime;
-	private float nextShotTimer;
+	public float nextProjectileTime;
+	private float npTimer;
 
 	protected override void Update() {
 		base.Update ();
-		updateNextShotTimer ();
-
+		updateNPTimer();
 	}
 
-	void updateNextShotTimer() {
-		nextShotTimer -= Time.deltaTime;
-		if (nextShotTimer < 0) {
-			nextShotTimer = 0;
+	void updateNPTimer() {
+		npTimer -= Time.deltaTime;
+		if (npTimer < 0) {
+			npTimer = 0;
 		}
 	}
 
 	public bool getNextShotReady() {
-		return nextShotTimer <= 0;
+		return npTimer <= 0;
 	}
 
-	public void resetNextShotTimer() {
-		nextShotTimer = nextShotTime;
+	/// <summary>
+	/// NP timer is the Next Projectile timer. This is the time it takes between one projectile to the next
+	/// </summary>
+	public void resetNPTimer() {
+		npTimer = nextProjectileTime;
 	}
+
+
 }
--- RebirthUnity2/Assets/Scripts/GameMechanics/WeaponLogic/GunsOrCannons/SkeletonBlaster.cs
+++ RebirthUnity2/Assets/Scripts/GameMechanics/WeaponLogic/GunsOrCannons/SkeletonBlaster.cs
@@ -3,52 +3,57 @@ using System.Collections;
 
 public class SkeletonBlaster : GunLogic {
 	private SkeletonBlasterStats blasterStats;
-	public float delayShot;
+	public float delayFireProjectile;
 	private float delayShotTimer;
 	private bool shotFired;
 
 	protected override void Start() {
 		base.Start ();
 		blasterStats = (SkeletonBlasterStats)baseGunStats;
+		delayShotTimer = delayFireProjectile;
+
+	}
+	protected override void Update() {
+		base.Update ();
+		checkShootProjectile ();
 	}
 
-	protected override void Update() {
 
-		base.Update ();
-		updateShootWeapon ();
+	/// <summary>
+	/// This method checks to see when it is appropirate to fire a projectile
+	/// </summary>
+	void checkShootProjectile() {
+		float previousTime = delayShotTimer;
+		delayShotTimer += Time.deltaTime;
+		if (delayShotTimer > delayFireProjectile) {
+			delayShotTimer = delayFireProjectile;
+		}
+		if (delayShotTimer >= delayFireProjectile && previousTime < delayShotTimer) {
+			GameObject obj = (GameObject)Instantiate(blasterStats.projectile.gameObject, 
+			                                         blasterStats.launchPositions[0].position, new Quaternion());
+			ProjectileStats bullet = obj.GetComponent<ProjectileStats>();
+			bullet.setWeaponOrigin(blasterStats);
+			if (blasterStats.weaponOwner.getIsRight()) {
+				bullet.setDirection(new Vector2(1, 0));
+			} else{
+				bullet.setDirection(new Vector2(-1, 0));
+			}
+
+		}
+
 	}
 
 	public override void fireWeapon() {
-		if (blasterStats.getNextShotReady()) {
-			shotFired = true;
-			delayShotTimer = delayShot;
-			blasterStats.resetNextShotTimer();
+		if (blasterStats.getNextShotReady ()) {
+			delayShotTimer = 0;
+			blasterStats.resetCoolDownTimer();
+			blasterStats.resetNPTimer ();
 		}
 	}
 
-	void updateShootWeapon() {
-		delayShotTimer -= Time.deltaTime;
-		if (delayShotTimer < 0) {
-			if (shotFired) {
-				blasterStats.resetNextShotTimer();
-				GameObject bullet = (GameObject)Instantiate(blasterStats.projectile.gameObject, 
-				                                            baseGunStats.launchPositions[0].position, new Quaternion());
-				bullet.GetComponent<ProjectileStats>().setWeaponOrigin(blasterStats);
-				if (blasterStats.weaponOwner.getIsRight()) {
-					bullet.GetComponent<ProjectileStats>().setDirection(new Vector2(1, 0));
-				}
-				else {
-					bullet.GetComponent<ProjectileStats>().setDirection(new Vector2(-1, 0));
-				}
-				baseGunStats.resetCoolDownTimer();
-				shotFired = false;
-			}
-			delayShotTimer = 0;
-		}
-	}
+
 
 	public override void reload() {
-
 
 	}
 }
